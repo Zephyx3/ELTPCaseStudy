1db20f02f1c4a6461aaee936c66ac172edb8e667
zephyx3 <zephyx3@gmail.com>
2020-03-03 14:43:37 +0800

    Initialize
    ELTP Case Study

---
+++ ELTPCaseStudy/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ELTPCaseStudy
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js", 
+                      "~/Scripts/jquery-{version}.js", 
+                      "~/Scripts/jquery.validate*", 
+                      "~/Scripts/umd/popper.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css", 
+                      "~/Content/Styles.css"));
+        }
+    }
+}
