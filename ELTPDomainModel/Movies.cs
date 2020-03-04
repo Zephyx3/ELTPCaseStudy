8a7454304cd70b2025b14d0631b1e309d9fe1d26
zephyx3 <zephyx3@gmail.com>
2020-03-04 15:13:19 +0800

    Domain Model Created
    - Movies
    - User
    
    -minor changes to index

---
+++ ELTPDomainModel/Movies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELTPDomainModel
+{
+    public class Movies
+    {
+        [Key]
+        public int MovieID { get; set; }
+        public string MovieName { get; set; }
+        public string MovieReleased { get; set; }
+        public int MovieReleaseDate { get; set; }
+        public int MovieGenre { get; set; }
+        public int MovieDescription { get; set; }
+    }
+}
