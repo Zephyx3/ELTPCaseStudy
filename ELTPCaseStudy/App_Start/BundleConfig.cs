d28371a56707dd3bec3d2628ea4d146cdfbb0110
zephyx3 <zephyx3@gmail.com>
2020-03-04 13:40:34 +0800

    Minor Changes to scripts
    Bundles Reverted to original
    Added bundles to _layout

--- ELTPCaseStudy/App_Start/BundleConfig.cs
+++ ELTPCaseStudy/App_Start/BundleConfig.cs
@@ -13,10 +13,14 @@ namespace ELTPCaseStudy
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js", 
-                      "~/Scripts/jquery-{version}.js", 
-                      "~/Scripts/jquery.validate*", 
                       "~/Scripts/umd/popper.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
