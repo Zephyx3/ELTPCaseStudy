dc289a9d379d27c010a98310e2f3fec8afb9bf54
unknown <tanjunwd17@gmail.com>
2020-03-04 19:18:56 +0800

    Added DB context ref

---
+++ ELTPDomainModel/ELTPDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ELTPDomainModel
+{
+    public class ELTPDbContext:DbContext
+    {
+        public DbSet<Movies> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users{ get; set; }
+    }
+}
