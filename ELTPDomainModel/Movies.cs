61302e8b73020eae5f45b85fe0634c0ce30715e8
zephyx3 <zephyx3@gmail.com>
2020-03-04 15:17:38 +0800

    Added Review & new fields

--- ELTPDomainModel/Movies.cs
+++ ELTPDomainModel/Movies.cs
@@ -13,8 +13,8 @@ namespace ELTPDomainModel
         public int MovieID { get; set; }
         public string MovieName { get; set; }
         public string MovieReleased { get; set; }
-        public int MovieReleaseDate { get; set; }
-        public int MovieGenre { get; set; }
-        public int MovieDescription { get; set; }
+        public DateTime MovieReleaseDate { get; set; }
+        public string MovieGenre { get; set; }
+        public string MovieDescription { get; set; }
     }
 }
---
+++ ELTPDomainModel/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELTPDomainModel
+{
+    public class Review
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReviewID { get; set; }
+        public string ReviewText { get; set; }
+        public DateTime ReviewDateAndTime { get; set; }
+        public int UserID { get; set; }
+        public int MovieID { get; set; }
+
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+    }
+}
