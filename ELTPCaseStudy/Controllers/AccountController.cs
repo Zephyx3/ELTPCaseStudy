9942701d941603ef98b44679dadabc176f4803f3
zephyx3 <zephyx3@gmail.com>
2020-03-05 11:56:02 +0800

    Created Additional Controllers
    - Account Controller
    - Rental Controller

---
+++ ELTPCaseStudy/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ELTPCaseStudy.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+
+        }
+
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
