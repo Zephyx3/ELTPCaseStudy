4a0c7ee07c49b53f420cf676d28d0ed22baef550
Zephy <zephyx3@gmail.com>
2020-03-05 17:53:47 +0800

    Added MapperExtensions
    - to ignore unmapped properties

---
+++ ELTPServiceLayer/MapperExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace ELTPServiceLayer
+{
+    public static class MapperExtensions
+    {
+        private static void IgnoreUnmappedProperties(TypeMap map, IMappingExpression expr)
+        {
+            //string[] propN = map.GetUnmappedPropertyNames();
+            foreach (string propName in map.GetUnmappedPropertyNames())
+            {
+
+                if (map.SourceType.GetProperty(propName) != null)
+                {
+                    expr.ForMember(propName, opt => opt.Ignore());
+                    //code below doesnt work
+                    //expr.ForSourceMember(propName, opt => opt.Ignore());
+                }
+                // new code below
+                if (map.DestinationType.GetProperty(propName) != null)
+                {
+                    expr.ForMember(propName, opt => opt.Ignore());
+                }
+            }
+        }
+        public static void IgnoreUnmapped(this IProfileExpression profile)
+        {
+            profile.ForAllMaps(IgnoreUnmappedProperties);
+
+        }
+    }
+}
