fb01eed5ff220f13bf9022feb681bc3bf5b2c789
Zephy <zephyx3@gmail.com>
2020-03-05 17:07:39 +0800

    Created ELTPRepositories & Implement UsersRepo Interface
    Repositories Created
    
    - Interfaces to be implemented for other items

--- ELTPRepository/UsersRepository.cs
+++ ELTPRepository/UsersRepository.cs
@@ -2,10 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ELTPDomainModel;
 
 namespace ELTPRepository
 {
-    public class UsersRepository
+    public interface IusersRepository
     {
+        void InsertUser(Users u);
+        void UpdateUser(Users u);
+        void UpdateUserDetails(Users u);
+        void UpdateUserPassword(Users u);
+        void DeleteUser(Users u);
+
+        List<Users> GetUsers();
+
+        List<Users> GetUsersByEmailAndPassowrd(string Email, string Password);
+
+        List<Users> GetUsersByEmail(string Email);
+        List<Users> GetUsersByID(int UserID);
+
+        int GetLatestUserID();
+    }
+    public class UsersRepository : IusersRepository
+    {
+        public void DeleteUser(Users u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetLatestUserID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetUsersByEmail(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetUsersByEmailAndPassowrd(string Email, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetUsersByID(int UserID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertUser(Users u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(Users u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUserDetails(Users u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUserPassword(Users u)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
