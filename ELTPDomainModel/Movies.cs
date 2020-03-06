005ace7a2307fa9d4444abf413c73827f45869da
Zephy <zephyx3@gmail.com>
2020-03-06 12:55:19 +0800

    Updated Domain Models
    - Movies.cs
    - Rental.cs

--- ELTPDomainModel/Movies.cs
+++ ELTPDomainModel/Movies.cs
@@ -11,10 +11,27 @@ namespace ELTPDomainModel
     {
         [Key]
         public int MovieID { get; set; }
+
         public string MovieName { get; set; }
+
         public string MovieReleased { get; set; }
+
         public DateTime MovieReleaseDate { get; set; }
+
         public string MovieGenre { get; set; }
+
         public string MovieDescription { get; set; }
+
+        public int ReviewsCount { get; set; }
+
+        public int RatingsCount { get; set; }
+
+        public int ViewsCount { get; set; }
+
+
+        [ForeignKey("UserID")]
+        public virtual Users Users { get; set; }
+
+        public virtual List<Reviews> Reviews { get; set; }
     }
 }
--- ELTPDomainModel/Reviews.cs
+++ ELTPDomainModel/Reviews.cs
@@ -9,14 +9,19 @@ namespace ELTPDomainModel
 {
     public class Reviews
     {
-        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ReviewID { get; set; }
-        public string ReviewName { get; set; }
+
         public string ReviewText { get; set; }
+
         public DateTime ReviewDateAndTime { get; set; }
+
         public int UserID { get; set; }
+
         public int MovieID { get; set; }
+
+        public int RatingsCount { get; set; }
+
 
         [ForeignKey("UserID")]
         public virtual Users User { get; set; }
